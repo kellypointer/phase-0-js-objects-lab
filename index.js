84b57b3f74e9b9b6a8fc82aa67fb9eee4708b78d
Kelly Pointer <kellyjoanpointer@gmail.com>
2025-02-15 22:29:57 -0600

    Objects lab

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 //Write your code here
 
+const attendee = {
+  attendeeID:  "T0001"
+  name = "Alice Smith"
+  event = "JavaScript Conference"
+  ticketType = "VIP"
+  ticketPrice = 150.00
+};
+
+
+function logAttendeeName(attendee) {
+  
+  console.log(attendee["attendee.name"); 
+}
+function logticketPrice(attendee) {
+  console.log(attendee.ticketPrice)
+}
+function updateTicketType(attendee) {
+  attendee.ticketType = "Regular";
+  console.log(attendee); 
+  
+}
+function removeEventProperty(attendee) {
+ 
+delete attendee.event; 
+}
+
+function addCheckedInProperty(attendee) {
+attendee.checkedIn = "True"
+
+}
+
+
 
 
 //Needed for the tests to work. Don't modify
